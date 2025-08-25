e5a9cd4ceaafe180124408f220c3e3674602a5fd
brr-songko <yhsong6837@gmail.com>
2025-08-25 16:39:09 +0900

    초기 세팅 main.js / mixins.js 추가

--- src/main.js
+++ src/main.js
@@ -1,5 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import mixins from './mixins'
+import store from './store';
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App);
+app.use(router);
+app.use(mixins);
+app.use(store);
+app.use(VueSweetalert2);
+app.mount('#app');
+
+window.Kakao.init("카카오 앱 키");
+
