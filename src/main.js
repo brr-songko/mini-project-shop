1a9b9818c0ba0a2b4d2f5b1cde582c236b0976d0
brr-songko <yhsong6837@gmail.com>
2025-08-26 15:15:23 +0900

    VueSweetAlert2 패키지 추가

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import router from './router'
 import mixins from './mixins'
 import store from './store';
+import VueSweetalert2 from "vue-sweetalert2";
+import 'sweetalert2/dist/sweetalert2.min.css';
 
 const app = createApp(App);
 app.use(router);
